1ceffdeca6c3f51370c02782a265d49c3dacc948
Priya <prithu12@yahoo.com>
2021-02-23 23:26:52 +0000

    Working copy

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const data = [
 
 //Global variables
 let selectedWordArray = [];
-let noOfAttempts = 6;
+let guessedWord = [];
+let noOfAttempts = 6; //one head + 1 body + 2 hands + 2 legs of hangman
+let attempt = 0;
 
 //Randomise the selection of word
 const createRandomWord = () => {
@@ -29,9 +31,10 @@ const createRandomWord = () => {
         const divElement = document.createElement('div');
         divElement.id = 'div-char-' + i;
         
-        if (selectedWordArray[i] === ' ')
+        if (selectedWordArray[i] === ' ') {
             textValue = ' ';
-        
+            guessedWord[i] = textValue; //add the space to guessedWord as user won't type space
+        }
         divElement.appendChild(document.createTextNode(textValue));    
         document.querySelector('.words').appendChild(divElement);
     }    
@@ -47,6 +50,14 @@ const createButtons = () => {
         button.classList.add('btn_letter');
         document.getElementById("letters").appendChild(button);
     }
+}
+
+//disable / enable buttons when game is finised or started resp
+const enableDisableLetters = (enableDisable) => {
+    let letterArray = document.getElementsByClassName('btn_letter');   
+
+    for(let i=0; i< letterArray.length; i++)
+        letterArray[i].disabled = enableDisable;
 }
 
 //Get the indexes of the character found in the selectedWordArray
@@ -70,20 +81,41 @@ const drawHangMan = (number) => {
     // audio.play();
 }
 
+const clearHangMan = () => {
+    let element = document.getElementsByClassName('hangman__fig')[0];
+
+    for (let i = 0; i < element.children.length; i++) {
+        element.children[i].style.display = 'none';
+    }
+}
+
 //Check if the letter pressed is found in the selectedWordArray and draw the hangman
 const checkLetter = (char, attempt) => {
     //check for char in the array
     let indexes = getAllIndexes(selectedWordArray, char);
-    console.log('indexes', indexes, char);
+    // console.log('indexes', indexes, char);
 
     if (indexes.length > 0) {
         indexes.forEach(index => {
             document.getElementById('div-char-' + index).innerHTML = char;
-        })
+            guessedWord[index] = char;
+        });
+        // console.log('guessedWord', guessedWord);
+    
+        if(guessedWord.join("") === selectedWordArray.join("")) {
+            displayResult("Congrats, Well Played! Click Play New to play again!!", "green");
+            enableDisableLetters(true);
+        }
+
         return true;
     } else {
         // if no char is found, start displaying the hangman
         drawHangMan(attempt);
+        if(attempt === noOfAttempts) {
+            displayResult("Better Luck Next time! Click Play New to play again!!", "red");
+            enableDisableLetters(true);
+        }
+            
         return false;
     }
     return true;
@@ -96,35 +128,38 @@ const displayResult = (message, color) => {
     element.style.color = color;
 }
 
+const playNewGame = () => {
+    //Re-initialize all variables and create a new random word to guess
+    attempt = 0; 
+    guessedWord = [];
+    clearHangMan();          
+    displayResult("",'white');
+    createRandomWord();
+    enableDisableLetters(false);
+}
+
 //On click events
 document.addEventListener('DOMContentLoaded', (event) => {
-    let attempt = 0;
-    console.log('loaded1');
     document.addEventListener('click', (event) => {
         var element = event.target;
 
-        console.log('element',element.tagName, element.className, element.id);
+        // console.log('element',element.tagName, element.className, element.id);
 
         if (element.tagName !== 'BUTTON'){       
             return;
         }
-        if (element.id === "btnPlay") {         
-            createRandomWord();
-            attempt = 0;
-            displayResult("",'white');
+        if (element.id === "btnPlay") {
+            playNewGame();
             return;
         }
 
         if (element.tagName === "BUTTON" && /btn_letter/.test(element.className)) {
             // console.log('btnclicked', element.innerHTML);
-            
+     
             if (noOfAttempts > attempt) {
-                if (!checkLetter(element.innerHTML, attempt+1)) 
-                    attempt++;
-                    console.log("attempt", attempt);
-            } else {
-                displayResult("Better Luck Next time! You have lost!!", "red");
-            };
+                if (!checkLetter(element.innerHTML.trim(), attempt+1)) 
+                    attempt++;        
+            } 
         }
     });
 });
